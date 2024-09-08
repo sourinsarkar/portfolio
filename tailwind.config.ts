ee5f68adcf60ee572b638647a21912126d651083
Sourin <iamsourinsarkar@gmail.com>
2024-09-08 17:13:59 +0530

    Set up initial file

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,18 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        pri: {
+          "100": "#FFFFFF",
+          "200": "#E6F9F1",
+          "300": "#CCDED6",
+          "400": "#B2C4BD",
+          "500": "#99ABA4",
+          "600": "#81928B",
+          "700": "#6A7A73",
+          "800": "#53635D",
+          "900": "#3E4D47",
+          "1000": "#293832",
+        },
       },
     },
   },
