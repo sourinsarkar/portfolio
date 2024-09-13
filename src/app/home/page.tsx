60523ee043ab148636f24b61773ee006e9ca7931
Sourin <iamsourinsarkar@gmail.com>
2024-09-13 19:19:37 +0530

    Implemented time module

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { RiArrowRightSLine, RiExternalLinkLine, RiTimeFill } from "@remixicon/react";
+import { RiArrowRightSLine, RiTimeFill } from "@remixicon/react";
 import NextIcon from "../../../public/icons/nexticon.svg";
 import ReactIcon from "../../../public/icons/reacticon.svg";
 import TailwindIcon from "../../../public/icons/tailwindicon.svg";
@@ -12,17 +12,42 @@ import CppIcon from "../../../public/icons/c++icon.svg";
 import PythonIcon from "../../../public/icons/pythonicon.svg";
 import FigmaIcon from "../../../public/icons/figmaicon.svg";
 import Image from "next/image";
-import Link from "next/link";
 import { ArrowTopRightOnSquareIcon } from "@heroicons/react/20/solid";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+    const [hour, setHour] = useState("");
+    const [minute, setMinute] = useState("");
+    const [second, setSecond] = useState("");
+    const [period, setPeriod] = useState("");
+
+    useEffect(() => {
+        const updateTime = () => {
+            const now = new Date();
+            const options = { timeZone: "Asia/Kolkata", hour12: true };
+            const timeString = now.toLocaleTimeString("en-US", options);
+            const [hour, minute, secondAndPeriod] = timeString.split(":");
+            const [second, period] = secondAndPeriod.split(" ");
+
+            setHour(hour.padStart(2, '0'));
+            setMinute(minute.padStart(2, '0'));
+            setSecond(second.padStart(2, '0'));
+            setPeriod(period);
+        };
+
+        updateTime();
+
+        const timer = setInterval(updateTime, 1000);
+        return () => clearInterval(timer);
+    }, []);
+
     const iconWidth = 40;
     const iconHeight = 40;
     return (
         <div className="space-y-16">
             <section className="flex flex-col items-center mt-16">
-                <div className="font-semibold flex flex-col items-center space-y-2">
-                    <p className="flex items-center"><RiTimeFill size={16} /> 12:05 PM</p>
+                <div className="font-semibold flex flex-col items-center space-y-4">
+                    <p className="flex items-center text-sm leading-none"><RiTimeFill size={16} /> {hour}:{minute} {period}</p>
                     <h1 className="text-6xl tracking-tight">Sourin Sarkar</h1>
                 </div>
                 <p className="text-lg mt-5">Hi there, I’m a developer and designer based in Bengaluru, India.</p>
