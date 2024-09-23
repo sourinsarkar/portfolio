cba4d1147788af26926349f98151e16d63746ca9
Sourin <iamsourinsarkar@gmail.com>
2024-09-23 17:49:40 +0530

    Implemented the project section

--- src/app/projects/components/ProjectItem.tsx
+++ src/app/projects/components/ProjectItem.tsx
@@ -2,22 +2,22 @@ import { RiArrowRightUpLine, RiCodeSSlashFill, RiGithubLine } from "@remixicon/r
 
 interface ProjectItemProps {
     project_name: string;
-    codelink?: string;
+    code_link?: string;
     code_status: boolean;
     link: string;
-}
+};
 
-export const ProjectItem: React.FC<ProjectItemProps> = ({ project_name, codelink, code_status, link }) => {
-    const iconSize = 20;
+export const ProjectItem: React.FC<ProjectItemProps> = ({ project_name, code_link, code_status, link }) => {
+    const iconSize = 18;
     return (
-        <li className="flex justify-between items-center group hover:text-[#0055FF] transition-colors duration-300">
-            <div className="text-lg font-semibold">{project_name}</div>
-            <div className="flex space-x-4">
+        <li className="flex justify-between items-center py-2.5 group hover:text-[#0055FF] transition-colors duration-300">
+            <div className="text-2xl font-semibold cursor-pointer">{project_name}</div>
+            <div className="flex space-x-3.5">
                 <a
-                    href={codelink}
+                    href={code_link}
                     target="_blank"
                     rel="noopener noreferrer"
-                    className={`transition-colors duration-300 ${code_status ? 'text-current group-hover:text-[#0055ff]' : "text-pri-400"}`}
+                    className={`bg-pri-50 group-hover:bg-[#dde9ff] rounded-full border-pri-50 p-2.5 transition-colors duration-300 ${code_status ? 'text-current group-hover:text-[#0055ff]' : "text-pri-400"}`}
                     >
                     <RiCodeSSlashFill size={iconSize} />
                 </a>
@@ -25,7 +25,7 @@ export const ProjectItem: React.FC<ProjectItemProps> = ({ project_name, codelink
                     href={link}
                     target="_blank"
                     rel="noopener noreferrer"
-                    className={`transition-colors duration-300 ${code_status ? 'text-current group-hover:text-[#0055ff]' : "text-pri-400"}`}
+                    className={`bg-pri-50 group-hover:bg-[#dde9ff] rounded-full border-pri-50 p-2.5 transition-colors duration-300 ${code_status ? 'text-current group-hover:text-[#0055ff]' : "text-pri-400"}`}
                 >
                     <RiArrowRightUpLine size={iconSize} />
                 </a>
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,7 +1,19 @@
+import { ProjectItem } from "./components/ProjectItem";
+
 export default function Projects() {
+    const projects = [
+        {project_name: "Fontground", code_link: "http://github.com/sourinsarkar/fontground", code_status: true, link: "https://fontground.sourin.in"},
+        {project_name: "Safepass", code_link: "http://github.com/sourinsarkar/safepass", code_status: true, link: "https://safepass.sourin.in"},
+        {project_name: "SPM", code_link: "http://github.com/sourinsarkar/spm", code_status: true, link: "https://spm.sourin.in"},
+    ];
+
     return (
         <div>
-            
+            <ul className="max-w-sm mx-auto mt-4">
+                {projects.map((project, index) => (
+                    <ProjectItem key={index} {...project} />
+                ))}
+            </ul>        
         </div>
     );
 }
