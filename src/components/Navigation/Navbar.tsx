cbd735faf046ad9eab73d8048dfa6dcdf266e388
Sourin <iamsourinsarkar@gmail.com>
2024-09-24 16:43:10 +0530

    Implemented Persistent Shadow on Navbar Items

--- src/components/Navigation/Navbar.tsx
+++ src/components/Navigation/Navbar.tsx
@@ -2,7 +2,8 @@
 
 import { RiArrowUpSLine, RiBrushLine, RiGithubLine, RiHome4Line, RiLightbulbFlashLine, RiMailLine, RiMoonLine, RiTwitterLine } from "@remixicon/react";
 import Link from "next/link";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { usePathname } from "next/navigation";
 
 export function Navbar() {
     const iconSize = 20;
@@ -13,22 +14,26 @@ export function Navbar() {
         setVisibility((previousValue) => (!previousValue));
     };
 
+    const pathname = usePathname();
+    const isActive = (path: string) => pathname === path;
+
     return (
         <nav className="inline-block fixed bottom-8 left-1/2 transform -translate-x-1/2 z-50">
-            <ul className="relative flex items-center NavList space-x-2">
-                <li><Link href="/home"><RiHome4Line size={iconSize} /></Link></li>
+            <ul className="flex items-center NavList space-x-2">
+                <li className={`${isActive('/home') ? 'shadow-is1' : ''}`}><Link href="/home"><RiHome4Line size={iconSize} /></Link></li>
                 <li><a target="_blank" href="https://dribbble.com/sourinsarkar"><RiBrushLine size={iconSize} /></a></li>
-                <li><Link href="/projects"><RiLightbulbFlashLine size={iconSize} /></Link></li>
+                <li className={`${isActive('/projects') ? 'shadow-is1' : ''}`}><Link href="/projects"><RiLightbulbFlashLine size={iconSize} /></Link></li>
                 <li><a target="_blank" href="https://github.com/sourinsarkar"><RiGithubLine size={iconSize} /></a></li>
                 <li className="hidden min-[500px]:block"><a target="_blank" href="https://x.com/sourin_inc"><RiTwitterLine size={iconSize} /></a></li>
                 <li className="hidden min-[500px]:block"><a target="_blank" href="mailto:work@sourin.in"><RiMailLine size={iconSize} /></a></li>
                 <li className="hidden min-[500px]:block"><RiMoonLine size={iconSize} /></li>
                 <li className="block min-[500px]:hidden" onClick={submenu}><RiArrowUpSLine size={iconSize} /></li>
-                <ul className={`absolute right-0 bottom-16 rounded-full p-1 border-pri-50 ring-1 ring-pri-50 bg-white space-y-2 ${visibility ? "block" : "hidden" }`}>
-                    <li><a target="_blank" href="https://x.com/sourin_inc"><RiTwitterLine size={iconSize} /></a></li>
-                    <li><a target="_blank" href="mailto:work@sourin.in"><RiMailLine size={iconSize} /></a></li>
-                    <li><RiMoonLine size={iconSize} /></li>
-                </ul>
+            </ul>
+
+            <ul className={`NavList rounded-full p-1 border-pri-50 ring-1 ring-pri-50 bg-white space-y-2 absolute right-0 bottom-16 ${visibility ? "block" : "hidden" }`}>
+                <li><a target="_blank" href="https://x.com/sourin_inc"><RiTwitterLine size={iconSize} /></a></li>
+                <li><a target="_blank" href="mailto:work@sourin.in"><RiMailLine size={iconSize} /></a></li>
+                <li><RiMoonLine size={iconSize} /></li>
             </ul>
         </nav>
     );
